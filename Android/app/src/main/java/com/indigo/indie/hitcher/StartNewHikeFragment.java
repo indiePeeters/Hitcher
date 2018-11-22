83ed66515fe4102b87e648a372e60d8e3662f046
DESKTOP-9O2LCNG\Indie <indiepeeters@hotmail.com>
2018-11-22 19:05:55 +0100

    Added basic animation

--- Android/app/src/main/java/com/indigo/indie/hitcher/StartNewHikeFragment.java
+++ Android/app/src/main/java/com/indigo/indie/hitcher/StartNewHikeFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 /**
  * Created by Indie on 11/14/2018.
@@ -19,6 +21,24 @@ public class StartNewHikeFragment  extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         currentView = inflater.inflate(R.layout.start_new_hike_layout, container, false);
+
+        //Listeners
+        Button button = (Button) currentView.findViewById(R.id.btn_startHike);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startHike(v);
+            }
+        });
+
         return currentView;
+    }
+
+    public void startHike(View view){
+        View textView =   currentView.findViewById(R.id.txtGoodLuck);
+        View animationView = currentView.findViewById(R.id.animation_view);
+        view.setEnabled(false);
+        textView.setVisibility(View.VISIBLE);
+        animationView.setVisibility(View.VISIBLE);
+        System.out.println("clicked");
     }
 }
